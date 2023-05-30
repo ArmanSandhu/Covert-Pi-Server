f76777806f369f8b142359728020dc7553a8e47d
Arman Sandhu <armanssandhu@gmail.com>
2023-05-29 17:56:09 -0700

    Security - Unpad Test Cases

--- internal/security/security_test.go
+++ internal/security/security_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestUnpad(t *testing.T) {
+	// Test Case 1
 	input := []byte{0x01, 0x02, 0x03, 0x03, 0x03, 0x03}
 	expectedOutput := []byte{0x01, 0x02, 0x03}
 	result := unpad(input)
@@ -13,6 +14,7 @@ func TestUnpad(t *testing.T) {
 		t.Errorf("Test case 1 failed. Expected: %v, go: %v", expectedOutput, result)
 	}
 
+	// Test Case 2
 	input = []byte{0x01, 0x02, 0x03, 0x03, 0x03, 0x03, 0x03}
 	expectedOutput = []byte{0x01, 0x02, 0x03, 0x03}
 	result = unpad(input)
@@ -20,6 +22,7 @@ func TestUnpad(t *testing.T) {
 		t.Errorf("Test case 2 failed. Expected: %v, go: %v", expectedOutput, result)
 	}
 
+	// Test Case 3
 	input = []byte{0x01, 0x02, 0x03, 0x03}
 	expectedOutput = []byte{0x01}
 	result = unpad(input)
@@ -27,6 +30,7 @@ func TestUnpad(t *testing.T) {
 		t.Errorf("Test case 3 failed. Expected: %v, go: %v", expectedOutput, result)
 	}
 
+	// Test Case 4
 	input = []byte{0x01, 0x02, 0x01}
 	expectedOutput = []byte{0x01, 0x02}
 	result = unpad(input)
