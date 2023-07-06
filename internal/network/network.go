6be8c8163cbceb3376b5b4c3d1800ba18f7e6499
Arman Sandhu <armanssandhu@gmail.com>
2023-07-06 16:08:25 -0700

    Network Functions and Tests Updated

--- internal/network/network.go
+++ internal/network/network.go
@@ -3,24 +3,18 @@ package network
 import (
 	"fmt"
 	"net"
-	"os"
 	"github.com/ArmanSandhu/CovertPi/internal/parsing"
 	"github.com/ArmanSandhu/CovertPi/internal/security"
 )
 
 var (
-	//ConnHost = "192.168.1.60"
-	//ConnPort = "8080"
-	//ConnType = "tcp"
 	shutdownChannel = make(chan struct{})
-	shutdownChannelPtr = &shutdownChannel
 )
 
 type listenerInterface interface {
 	Accept() (net.Conn, error)
 	Close() error
 	Addr() net.Addr
-	//Listen(network, address string) (net.Listener, error)
 	Listen() (net.Listener, error)
 }
 
@@ -36,6 +30,7 @@ func (l *Listener) Listen() (net.Listener, error) {
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println("Listening on " + l.ConnHost + ":" + l.ConnPort)
 	l.Listener = listener
 	return listener, nil
 }
@@ -56,32 +51,36 @@ func (l *Listener) Accept() (net.Conn, error) {
 	return conn, nil
 }
 
-func StartServer(listen listenerInterface) {
+func StartServer(listen listenerInterface) (string, error) {
+	// Start a routine to watch for a shutdown signal
+	go func() {
+		// Wait for a shutdown signal
+		<-shutdownChannel
+		// Stop the Server
+		StopServer()
+	}()
+
 	//Listen for incoming connection
-	//listener, err := net.Listen(ConnType, ConnHost + ":" + ConnPort)
-	//listener, err := listen.Listen(listen.ConnType, listen.ConnHost + ":" + listen.ConnPort)
 	listener, err := listen.Listen()
 	if err != nil {
 		fmt.Println("Error Listening: ", err.Error())
-		os.Exit(1)
+		return "Error", err
 	}
 	// Close listener when program finishes
 	defer listener.Close()
-
-	fmt.Println("Listening on " + ConnHost + ":" + ConnPort)
 	
 	// Wait for incoming connections
 	for {
 		select {
-		case <- shutdownChannel:
+		case <-shutdownChannel:
 			//Signal server to stop receiving new connections
-			StopServer()
-			return
+			fmt.Println("Server Stopped!")
+			return "Stop", nil
 		default:
 			conn, err := listener.Accept()
 			if err != nil {
 				fmt.Println("Error Accepting: ", err.Error())
-				os.Exit(1)
+				return "Error", err
 			}
 			go handleInConn(conn)
 		}
@@ -97,17 +96,24 @@ func handleInConn(conn net.Conn) {
 	buf := make([]byte, 1024)
 	for {
 		reqLen, err := conn.Read(buf[:cap(buf)])
-		buf = buf[:reqLen]
 		if err != nil {
 			fmt.Println("Error Accepting: ", err.Error())
 			break;
 		}
-		fmt.Printf("Read %d bytes\n", reqLen)
-		encCmdString := string(buf)
-		fmt.Println("Incoming Enc String: ", encCmdString)
-		fmt.Println("Beginning Decryption!")
-		cmdString := security.Decrypt(encCmdString)
-		fmt.Println("Decrypted Cmd String: ", cmdString)
-		parsing.RunCommand(conn, cmdString)
+		if reqLen > 0 {
+			buf = buf[:reqLen]
+			fmt.Printf("Read %d bytes\n", reqLen)
+			encCmdString := string(buf)
+			fmt.Println("Incoming Enc String: ", encCmdString)
+			fmt.Println("Beginning Decryption!")
+			cmdString := security.Decrypt(encCmdString)
+			fmt.Println("Decrypted Cmd String: ", cmdString)
+			parsing.RunCommand(conn, cmdString)
+		}
+
+		if reqLen == 0 {
+			fmt.Println("Connection Closed!")
+			break
+		}
 	}
 }
---
+++ internal/network/network_test.go
@@ -0,0 +1,103 @@
+package network
+
+import (
+	"testing"
+	"fmt"
+	"net"
+	"time"
+	"sync"
+)
+
+// Setup mock structs for testing
+type mockConnection struct{
+	remoteAddr net.Addr
+}
+type mockListener struct {
+	ConnHost string
+	ConnPort string
+	ConnType string
+}
+// ----MOCK LISTENER FUNCTIONS----
+func (ml *mockListener) Accept() (net.Conn, error) {
+	fmt.Println("Accepted Incoming Connection!")
+	time.Sleep(time.Millisecond)
+	return &mockConnection{}, nil
+}
+
+func (ml *mockListener) Close() error {
+	fmt.Println("Closing Client Connection!")
+	time.Sleep(time.Millisecond)
+	return nil
+}
+
+func (ml *mockListener) Addr() net.Addr {
+	return &net.IPAddr{
+		IP: net.ParseIP(ml.ConnHost),
+	}
+}
+
+func (ml *mockListener) Listen() (net.Listener, error) {
+	return &mockListener{}, nil
+} 
+
+// ----MOCK CONNECTION FUNCIONS----
+func (mc *mockConnection) Read(b []byte) (n int, err error) {
+	return 0, nil
+}
+
+func (mc *mockConnection) Write(b []byte) (n int, err error) {
+	return 0, nil
+}
+
+func (mc *mockConnection) Close() error {
+	return nil
+}
+
+func (mc *mockConnection) LocalAddr() net.Addr {
+	return nil
+}
+
+func (mc *mockConnection) RemoteAddr() net.Addr {
+	return mc.remoteAddr
+}
+
+func (mc *mockConnection) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (mc *mockConnection) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (mc *mockConnection) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+
+func TestStartServer(t *testing.T) {
+	listener := &mockListener{}
+	
+	var err error
+	var actualOutput string
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		actualOutput, err = StartServer(listener)
+	}()
+
+	time.Sleep(time.Millisecond)
+
+	shutdownChannel <- struct{}{}
+	fmt.Println("Shutdown Signal Sent")
+	wg.Wait()
+	
+	if err != nil {
+		t.Errorf("Error occured in StartServer: %v", err)
+	}
+
+	expectedOutput := "Stop"
+	if actualOutput != expectedOutput {
+		t.Errorf("Expected Server Output: %s, but got: %s\n", expectedOutput, actualOutput)
+	}
+}
